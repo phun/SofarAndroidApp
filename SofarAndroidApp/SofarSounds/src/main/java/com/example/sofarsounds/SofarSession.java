4351ce7547e5258bf02b4242d07afbecd7dfd886
Frank <fvs@mit.edu>
2014-04-07 04:42:02 -0400

    Session handler object.

---
+++ SofarAndroidApp/SofarSounds/src/main/java/com/example/sofarsounds/SofarSession.java
@@ -0,0 +1,55 @@
+package com.example.sofarsounds;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by lucid on 4/7/14.
+ */
+public class SofarSession {
+    private final String sessionID;
+    private final Context context;
+    private SofarSession(Context context, String sessionID) {
+        this.context = context;
+        this.sessionID = sessionID;
+    }
+
+    private static SharedPreferences getStorage(Context ctx) {
+        return ctx.getSharedPreferences(ctx.getString(R.string.session_preference_id), Context.MODE_PRIVATE);
+    }
+
+    public static SofarSession getCurrentSession(Context ctx) {
+        SharedPreferences storage = getStorage(ctx);
+        if (storage.contains("session_id"))
+            return new SofarSession(ctx, storage.getString("session_id", null));
+        else
+            return null;
+    }
+
+    public static boolean hasValidSession(Context ctx) {
+        // TODO: Issue a server call to ensure current session is valid.
+        return getCurrentSession(ctx) != null;
+    }
+
+    public static SofarSession openNewSession(Context ctx, String username, String password) {
+        if (getCurrentSession(ctx) != null)
+            getCurrentSession(ctx).close();
+        SofarSession sess = new SofarSession(ctx, "UPDATE WITH SERVER RANDOM STRING HERE");
+        SharedPreferences.Editor e = getStorage(ctx).edit();
+        e.putString("session_id", sess.sessionID);
+        return sess;
+    }
+
+    public void close() {
+        // TODO: Issue a logout / invalidate session on server.
+        // if (this.equals(getCurrentSession())) { OOH WEIRD RACE CONDITION
+        SharedPreferences.Editor e = getStorage(context).edit();
+        e.remove("session_id");
+    }
+
+    public boolean equals(Object other) {
+        if (!(other instanceof SofarSession))
+            return false;
+        return ((SofarSession)other).sessionID.equals(sessionID);
+    }
+}
